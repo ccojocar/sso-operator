d24e19db0c179c922ab8b1fe252afb34c9aa7948
Cosmin Cojocar <cosmin.cojocar@gmx.ch>
2018-07-24 13:56:49 +0200

    fix:(operator) start a health endpoint on 8080 port

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"net/http"
 	"os"
 	"runtime"
 
@@ -21,10 +22,24 @@ func printVersion(namespace string) {
 	logrus.Infof("operator namespace: %s", namespace)
 }
 
+func handleHealth() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		logrus.Debug("ping")
+	})
+	http.ListenAndServe(":8080", nil)
+}
+
 func main() {
 	ns := os.Getenv(operatorNamespace)
 	printVersion(ns)
+
+	// configure the operator
 	sdk.Watch("sso.jenkins.io/v1", "SSO", ns, 5)
 	sdk.Handle(operator.NewHandler())
+
+	// start the health probe
+	go handleHealth()
+
+	// start the operator
 	sdk.Run(context.TODO())
 }
