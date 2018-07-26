1fb03e73dfa6586c4b6d3e66e1f5e21c4d281b6b
Cosmin Cojocar <cosmin.cojocar@gmx.ch>
2018-07-26 14:53:43 +0200

    feat:(dex) implement a gRPC client for dex

--- main.go
+++ main.go
@@ -2,19 +2,32 @@ package main
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"net/http"
 	"os"
 	"runtime"
 
+	"github.com/jenkins-x/sso-operator/pkg/dex"
 	"github.com/jenkins-x/sso-operator/pkg/operator"
 	sdk "github.com/operator-framework/operator-sdk/pkg/sdk"
 	sdkVersion "github.com/operator-framework/operator-sdk/version"
 
 	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
 )
 
 const operatorNamespace = "OPERATOR_NAMESPACE"
 const port = "8080"
+
+// OperatorOptions holds the command options for SSO operator
+type OperatorOptions struct {
+	Namespace          string
+	DexGrpcHostAndPort string
+	DexGrpcCA          string
+	DexGrpcClientCrt   string
+	DexGrpcClientKey   string
+}
 
 func printVersion(namespace string) {
 	logrus.Infof("Go Version: %s", runtime.Version())
@@ -34,17 +47,79 @@ func handleLiveness() {
 	}
 }
 
-func main() {
-	ns := os.Getenv(operatorNamespace)
+// Run starts the SSO operator
+func (o *OperatorOptions) Run() {
+	ns := o.Namespace
+	if ns == "" {
+		ns = os.Getenv(operatorNamespace)
+	}
 	printVersion(ns)
+
+	// validate the command line options
+	err := o.Validate()
+	if err != nil {
+		logrus.Errorf("invalid options: %v", err)
+		os.Exit(2)
+	}
+
+	dexClient, err := dex.NewClient(o.DexGrpcHostAndPort, o.DexGrpcCA, o.DexGrpcClientCrt, o.DexGrpcClientKey)
+	if err != nil {
+		logrus.Errorf("failed to crate dex client: %v", err)
+		os.Exit(2)
+	}
 
 	// configure the operator
 	sdk.Watch("sso.jenkins.io/v1", "SSO", ns, 5)
-	sdk.Handle(operator.NewHandler())
+	sdk.Handle(operator.NewHandler(dexClient))
 
 	// start the health probe
 	go handleLiveness()
 
 	// start the operator
 	sdk.Run(context.TODO())
+}
+
+// Validate validates the provided command options
+func (o *OperatorOptions) Validate() error {
+	if o.DexGrpcHostAndPort == "" {
+		return errors.New("dex gRPC server host and port is empty")
+	}
+	if _, err := os.Stat(o.DexGrpcCA); os.IsNotExist(err) {
+		return fmt.Errorf("provided dex gRPC CA file '%s' does not exist", o.DexGrpcCA)
+	}
+	if _, err := os.Stat(o.DexGrpcClientCrt); os.IsNotExist(err) {
+		return fmt.Errorf("provided dex gRPC client cert file '%s' does not exist", o.DexGrpcClientCrt)
+	}
+
+	if _, err := os.Stat(o.DexGrpcClientKey); os.IsNotExist(err) {
+		return fmt.Errorf("provided dex gRPC client key file '%s' does not exists", o.DexGrpcClientKey)
+	}
+
+	return nil
+}
+
+func commandRoot() *cobra.Command {
+	options := &OperatorOptions{}
+
+	rootCmd := &cobra.Command{
+		Use: "sso-operator",
+		Run: func(cmd *cobra.Command, args []string) {
+			options.Run()
+		},
+	}
+
+	rootCmd.Flags().StringVarP(&options.Namespace, "namespace", "n", "", "Namespace where the operator will watch for resources")
+	rootCmd.Flags().StringVarP(&options.DexGrpcHostAndPort, "dex-grpc-host-port", "", "", "Host and port of Dex gRPC server")
+	rootCmd.Flags().StringVarP(&options.DexGrpcCA, "dex-grpc-ca", "", "", "CA for Dex gRPC server and client")
+	rootCmd.Flags().StringVarP(&options.DexGrpcClientCrt, "dex-grpc-client-crt", "", "", "Certificate for Dex gRPC client")
+	rootCmd.Flags().StringVarP(&options.DexGrpcClientKey, "dex-grpc-client-key", "", "", "Key for Dex gRPC client")
+
+	return rootCmd
+}
+
+func main() {
+	if err := commandRoot().Execute(); err != nil {
+		logrus.Error(err)
+		os.Exit(2)
+	}
 }
---
+++ pkg/dex/client.go
@@ -0,0 +1,91 @@
+package dex
+
+import (
+	"context"
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
+	"io/ioutil"
+
+	"github.com/coreos/dex/api"
+	"github.com/pkg/errors"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+)
+
+// Client represent a client wrapper for Dex
+type Client struct {
+	dex api.DexClient
+}
+
+// NewClient creates a new Dex client
+func NewClient(hostAndPort, caPath, clientCrt, clientKey string) (*Client, error) {
+	certPool := x509.NewCertPool()
+	caCert, err := ioutil.ReadFile(caPath) // #nosec
+	if err != nil {
+		return nil, errors.Wrapf(err, "cannot open the CA file with the path '%s'", caPath)
+	}
+	appended := certPool.AppendCertsFromPEM(caCert)
+	if !appended {
+		return nil, errors.New("failed to append the CA cert to the certs pool")
+	}
+
+	clientCert, err := tls.LoadX509KeyPair(clientCrt, clientKey)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to load the client cert '%s' and key '%s'", clientCrt, clientKey)
+	}
+
+	clientTLSConfig := &tls.Config{
+		RootCAs:      certPool,
+		Certificates: []tls.Certificate{clientCert},
+	}
+	creds := credentials.NewTLS(clientTLSConfig)
+
+	conn, err := grpc.Dial(hostAndPort, grpc.WithTransportCredentials(creds))
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to open the gRPC connection with server '%s'", hostAndPort)
+	}
+	return &Client{
+		dex: api.NewDexClient(conn),
+	}, nil
+}
+
+// CreateClient a new OIDC client in Dex
+func (c *Client) CreateClient(ctx context.Context, redirectUris []string, trustedPeers []string,
+	public bool, name string, logoURL string) (*api.Client, error) {
+	req := &api.CreateClientReq{
+		Client: &api.Client{
+			RedirectUris: redirectUris,
+			TrustedPeers: trustedPeers,
+			Public:       public,
+			Name:         name,
+			LogoUrl:      logoURL,
+		},
+	}
+
+	res, err := c.dex.CreateClient(ctx, req)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to create the OIDC client")
+	}
+
+	if res.AlreadyExists {
+		return nil, errors.Wrapf(err, "client '%s' already exists", res.Client.Id)
+	}
+
+	return res.Client, nil
+}
+
+// DeleteClient deletes the client with given Id from Dex
+func (c *Client) DeleteClient(ctx context.Context, id string) error {
+	req := &api.DeleteClientReq{
+		Id: id,
+	}
+	res, err := c.dex.DeleteClient(ctx, req)
+	if err != nil {
+		return errors.Wrapf(err, "failed to delete the client with id '%s'", id)
+	}
+	if res.NotFound {
+		return fmt.Errorf("delete did not found the client with id '%s'", id)
+	}
+	return nil
+}
